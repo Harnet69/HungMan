3c5857930f4b31e08dbf36436576325aec3c1c4f
Harnet69 <harnet69@gmail.com>
2020-02-10 13:36:42 +0100

    Initial commit

---
+++ src/com/harnet/InputValidator.java
@@ -0,0 +1,4 @@
+package com.harnet;
+
+public class InputValidator {
+}
---
+++ src/com/harnet/UserInput.java
@@ -0,0 +1,12 @@
+package com.harnet;
+
+public class UserInput {
+    String name;
+    private int attemptsNum;
+
+    public UserInput(int attemptsNum) {
+        this.attemptsNum = attemptsNum;
+    }
+
+
+}
